0020dd7ee63514044f28f13e0e6c3918c7b252ba
li6185377 <linyunfeng@ljh-341.local>
2014-03-05 12:00:13 +0800

    加入了 抖动效果

--- WaterDropTest/waterDrop/SYWaterDropView.m
+++ WaterDropTest/waterDrop/SYWaterDropView.m
@@ -8,6 +8,14 @@
 
 #import "SYWaterDropView.h"
 
+#ifndef RGBCOLOR
+#define RGBCOLOR(r,g,b) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]
+#endif
+
+#ifndef RGBCOLORA
+#define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
+#endif
+
 @interface SYWaterDropView()
 {
     float _minOffset;
@@ -16,7 +24,7 @@
     BOOL _animationing;
     BOOL _isRefresh;
 }
-
+@property(strong,nonatomic)CAShapeLayer* doudongLayer;
 @property(strong,nonatomic)NSTimer* timer;
 @end
 
@@ -53,16 +61,24 @@
     self.frame = frame;
     
     _lineLayer = [CAShapeLayer layer];
-    _lineLayer.fillColor = [UIColor colorWithRed:222./255. green:216./255. blue:211./255. alpha:0.5].CGColor;
-    
+    _lineLayer.fillColor = RGBACOLOR(222, 216, 211, 0.5).CGColor;
     [self.layer addSublayer:_lineLayer];
     
     
     _shapeLayer = [CAShapeLayer layer];
-    _shapeLayer.fillColor = [UIColor colorWithRed:222./255. green:216./255. blue:211./255. alpha:1].CGColor;
+    _shapeLayer.fillColor = RGBCOLOR(222, 216, 211).CGColor;
     _shapeLayer.strokeColor = [[UIColor whiteColor] CGColor];
     _shapeLayer.lineWidth = 3;
     [self.layer addSublayer:_shapeLayer];
+    
+    _doudongLayer = [CAShapeLayer layer];
+    _doudongLayer.fillColor = RGBCOLOR(222, 216, 211).CGColor;
+    _doudongLayer.strokeColor = [[UIColor whiteColor] CGColor];
+    _doudongLayer.lineWidth = 3;
+    _doudongLayer.frame = CGRectMake(15-_radius,self.bounds.size.height-20-_radius*2, _radius*2, _radius*2);
+    _doudongLayer.path = CGPathCreateWithEllipseInRect(_doudongLayer.bounds, NULL);
+    _doudongLayer.opacity = 0;
+    [self.layer addSublayer:_doudongLayer];
     
     self.currentOffset = 0;
 }
@@ -70,7 +86,7 @@
 -(CGMutablePathRef)createPathWithOffset:(float)currentOffset
 {
     CGMutablePathRef path = CGPathCreateMutable();
-    float top = self.frame.size.height - 20 - _radius*2 - currentOffset;
+    float top = self.bounds.size.height - 20 - _radius*2 - currentOffset;
     float wdiff = currentOffset* 0.2;
     
     if(currentOffset==0)
@@ -111,7 +127,7 @@
     if(currentOffset < _maxOffset)
     {
         float wdiff = currentOffset* 0.2;
-        float top = self.frame.size.height - 20 - _radius*2 - currentOffset;
+        float top = self.bounds.size.height - 20 - _radius*2 - currentOffset;
         
         CGMutablePathRef path = [self createPathWithOffset:currentOffset];
         _shapeLayer.path = path;
@@ -120,10 +136,24 @@
         
         CGMutablePathRef line = CGPathCreateMutable();
         float w = ((_maxOffset - currentOffset)/_maxOffset) + 1;
-        CGPathAddRect(line, NULL, CGRectMake(15-w/2, top + wdiff + _radius*2,w, currentOffset-wdiff+20));
+        if(currentOffset==0)
+        {
+            CGPathAddRect(line, NULL, CGRectMake(15-w/2, top + wdiff + _radius*2,w, currentOffset-wdiff+20));
+        }
+        else{
+            float lt = top+wdiff+_radius*2;
+            float lb = currentOffset-wdiff+20 + lt;
+            CGPathMoveToPoint(line, NULL, 15- w/2,lt);
+            CGPathAddLineToPoint(line, NULL, 15 + w/2,lt);
+            
+            CGPathAddCurveToPoint(line, NULL,15 + w/2,lt, 15 + w/2,lt+10, 15+1 , lb);
+            CGPathAddLineToPoint(line,  NULL, 15 - 1, lb);
+            CGPathAddCurveToPoint(line, NULL,15 - w/2,lt, 15 - w/2,lt+10,15-w/2, lt);
+        }
+        CGPathCloseSubpath(line);
         _lineLayer.path = line;
-        
-        self.transform = CGAffineTransformMakeScale(0.8+0.2*(w-1), 1);
+        CGPathRelease(line);
+        self.transform = CGAffineTransformMakeScale(0.85+0.15*(w-1), 1);
     }
     else
     {
@@ -149,7 +179,7 @@
         {
             self.handleRefreshEvent();
         }
-        [self startRefreshAnimation];
+        [self doudong];
     }
 }
 -(void)stopRefresh
@@ -164,7 +194,7 @@
     [_refreshView.layer addAnimation:anim forKey:nil];
     _refreshView.layer.opacity = 0;
     
-
+    
     anim = [CABasicAnimation animationWithKeyPath:@"opacity"];
     anim.fromValue = @(0);
     anim.toValue = @(1);
@@ -185,6 +215,7 @@
         [_refreshView.layer removeAllAnimations];
     }
 }
+
 -(void)startRefreshAnimation
 {
     if(self.refreshView == nil)
@@ -192,9 +223,11 @@
         self.refreshView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"first_timetowlight"]];
         [self addSubview:_refreshView];
     }
+    
+    _doudongLayer.opacity = 0;
     _shapeLayer.opacity = 0;
     
-    _refreshView.center = CGPointMake(15,self.frame.size.height - 20 - _radius);
+    _refreshView.center = CGPointMake(15,self.bounds.size.height - 20 - _radius);
     [_refreshView.layer removeAllAnimations];
     _refreshView.layer.opacity = 1;
     
@@ -205,5 +238,68 @@
     animation.repeatCount = INT_MAX;
     
     [_refreshView.layer addAnimation:animation forKey:@"rotation"];
+}
+
+-(CGPoint)scaleWithIndex:(int)i
+{
+    float xvalue = 1 - (0.4f- i*0.1f)*(2 * i%2 - 1);
+    float yvalue = 1 + (0.4f- i*0.1f)*(2 * i%2 - 1);
+    
+    if(i%2==0){
+        return CGPointMake(yvalue, xvalue);
+    }
+    else{
+        return CGPointMake(xvalue, yvalue);
+    }
+}
+
+NS_INLINE CATransform3D CATransform3DMakeRotationScale(CGFloat angle,CGFloat sx, CGFloat sy,
+                                                       CGFloat sz)
+{
+    CATransform3D transform = CATransform3DIdentity;
+    transform = CATransform3DRotate(transform, angle, 0, 0, 1);
+    transform = CATransform3DScale(transform, sx, sy, sz);
+    transform = CATransform3DRotate(transform, -angle, 0, 0, 1);
+    return transform;
+}
+-(void)doudong
+{
+    _doudongLayer.opacity = 1;
+    _shapeLayer.opacity = 0;
+    
+    float kDuration = 0.5;
+    float angle = M_PI;
+    float w = 1.3 , h = 1.3;
+    
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    animation.values = [NSArray arrayWithObjects:
+                        [NSValue valueWithCATransform3D:_doudongLayer.transform],
+                        [NSValue valueWithCATransform3D:CATransform3DMakeRotationScale(angle,w, 2-h, 1)],
+                        [NSValue valueWithCATransform3D:CATransform3DMakeRotationScale(angle,1-(w-1)*0.5, 1+(h-1)*0.5, 1)],
+                        [NSValue valueWithCATransform3D:CATransform3DMakeRotationScale(angle,1+(w-1)*0.25, 1-(h-1)*0.25, 1)],
+                        [NSValue valueWithCATransform3D:CATransform3DMakeRotationScale(angle,1, 1, 1)], nil];
+    animation.keyTimes = [NSArray arrayWithObjects:
+                          [NSNumber numberWithFloat:0],
+                          [NSNumber numberWithFloat:0.3],
+                          [NSNumber numberWithFloat:0.6],
+                          [NSNumber numberWithFloat:0.9],
+                          [NSNumber numberWithFloat:1], nil];
+    _doudongLayer.transform = CATransform3DIdentity;
+    animation.timingFunctions = [NSArray arrayWithObjects:
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut],
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut], nil];
+    
+    animation.duration = kDuration;
+    [_doudongLayer addAnimation:animation forKey:nil];
+    
+    __weak SYWaterDropView* wself = self;
+    
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(kDuration * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        wself.doudongLayer.opacity = 0;
+        [wself startRefreshAnimation];
+    });
 }
 @end
